e0abb340073996ec2c4d4386b8a99b26117f05e9
Karan Singh <kar.n@icloud.com>
2017-05-15 21:29:49 -0700

    Full skeleton including socket.io is now up and running.

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-const express = require('express'),
-      app     = express(),
-      path    = require('path'),
-      port    = process.env.PORT || 3000;
+const express     = require('express'),
+      http        = require('http'),
+      socketIO    = require('socket.io'),
+      app         = express(),
+      path        = require('path'),
+      port        = process.env.PORT || 3000,
+      publicPath  = path.join(__dirname, '/public');
 
-const publicPath = path.join(__dirname, '/public');
+const server      = http.createServer(app);
+const io          = socketIO(server);
 //serving public folder
 app.use(express.static(publicPath));
 
+// Socket.io events
+io.on('connection', (socket)=>{
+    console.log('new user connected');
+    socket.emit('newMessage',{
+        from: "@karann7",
+        text: "Hello socketio",
+        createAt: 456456
+    });
+    socket.on('createMessage', (message)=>{
+    console.log('The message is : ', message);
+    });
+    socket.on('disconnect', ()=>{
+        console.log('User was disconnected');
+    });
+});
+
+
 //server listen
-app.listen(port, ()=>{
+server.listen(port, ()=>{
       console.log(`SERVER IS RUNNING ON PORT: ${port}`);
 });
---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+//requiring the socketio js file that comes built in socket.io framework
+var socket = io();
+//Socket.io events
+socket.on('connect', function(){
+  console.log('connected to server');
+
+  socket.emit('createMessage', {
+    to: '@apottr',
+    text: 'Hey. this is Karan'
+  });
+});
+  socket.on('newMessage', function(message){
+    console.log(JSON.stringify(message, undefined, 2));
+  });
+
+socket.on('disconnect', function(){
+  console.log('Disconnected from the server');
+});
