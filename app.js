141a7ca33f69a46f24e3a1df958c77e2fcf6a8de
Karan Singh <kar.n@icloud.com>
2017-05-16 00:24:55 -0700

    added comments to help read code

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
+//NPM dependcies
 const express     = require('express'),
       http        = require('http'),
       socketIO    = require('socket.io'),
       app         = express(),
-      path        = require('path'),
-      port        = process.env.PORT || 3000,
+      path        = require('path');
+// local dependencies
+const port        = process.env.PORT || 3000,
       publicPath  = path.join(__dirname, '/public'),
       generateMessage = require('./utils/message.js');
-
+//the express app gets passed in the http method
+//the http method then gets passed into the socket.io
 const server      = http.createServer(app);
 const io          = socketIO(server);
 //serving public folder
 app.use(express.static(publicPath));
-
-// Socket.io events
+// All the events must be included in the io.on block
 io.on('connection', (socket)=>{
     console.log('new user connected');
-
+    //sends welcome message upon page entry
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-
+    //sends  message to everyone but the current socket.
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+   //takes a incoming message, and sends it to other connected sockets
     socket.on('createMessage', (message)=>{
     console.log('The message is : ', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
