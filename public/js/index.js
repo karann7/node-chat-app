9f6335eb3e0eb654bf9928a82e6c0a64c8cf2895
Karan Singh <kar.n@icloud.com>
2017-05-16 01:24:59 -0700

    added jQuery to project for DOM manipulation

--- public/js/index.js
+++ public/js/index.js
@@ -11,11 +11,20 @@ socket.on('disconnect', function(){
 
 socket.on('newMessage', function(message){
     console.log(JSON.stringify(message, undefined, 2));
+    var li = $('<li></li>');
+    li.text(message.from + ": " + message.text);
+    $('#messages').append(li);
 });
 
+
+
+$('#message-form').on('submit', function(e){
+  //prevents the form from refreshing the page and adding value to URL
+  e.preventDefault(); 
 socket.emit('createMessage', {
-  from: 'Karan',
-  text: 'heheheheh'
+  from: 'User',
+  text: $('[name=message]').val()
 }, function(data){
   console.log('Delivered!' + " "+ data);
+});
 });
