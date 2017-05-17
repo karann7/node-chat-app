c777079a8fae85054940f13da3844f6e2e94092d
Karan Singh <kar.n@icloud.com>
2017-05-16 17:12:12 -0700

    using mustachejs for templating rather than adding items manually via jquery

--- public/js/index.js
+++ public/js/index.js
@@ -1,58 +1,66 @@
-//requiring the socketio js file that comes built in socket.io framework
 var socket = io();
-//Socket.io events
-socket.on('connect', function(){
-  console.log('connected to server');
-});
- 
-socket.on('disconnect', function(){
-  console.log('Disconnected from the server');
+
+socket.on('connect', function () {
+  console.log('Connected to server');
 });
 
-socket.on('newMessage', function(message){
-    var formatTime = moment(message.createdAt).format('h:mm a');
-    var li = $('<li></li>');
-    li.text(message.from  + " " +formatTime+ ": " + message.text);
-    $('#messages').append(li);
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
 });
 
-socket.on('newLocationMessage', function(message){
-  var formatTime = moment(message.createdAt).format('h:mm a');
-  var li = $('<li></li>');
-  var a  = $('<a target="_blank">My current location</a>');
-  li.text(message.from + " " + formatTime + ": ");
-  a.attr('href', message.url);
-  li.append(a);
-  $('#messages').append(li);
+socket.on('newMessage', function (message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
 });
 
+socket.on('newLocationMessage', function (message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
 
-$('#message-form').on('submit', function(e){
-  //prevents the form from refreshing the page and adding value to URL
-  e.preventDefault(); 
-  var messageTextbox = $('[name=message]');
+  jQuery('#messages').append(html);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  var messageTextbox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
     text: messageTextbox.val()
-    }, function(){
-    messageTextbox.val(''); 
+  }, function () {
+    messageTextbox.val('')
   });
 });
 
-var locationButton = $('#send-location');
-locationButton.on('click', function(){
-  if(!navigator.geolocation){
-    return alert('Sorry Geolocation in your browser is not supported');
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
   }
+
   locationButton.attr('disabled', 'disabled').text('Sending location...');
-  navigator.geolocation.getCurrentPosition(function(position){
+
+  navigator.geolocation.getCurrentPosition(function (position) {
     locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
-  }, function(){
-    alert('Unable to fetch your location');
+  }, function () {
     locationButton.removeAttr('disabled').text('Send location');
+    alert('Unable to fetch location.');
   });
 });
