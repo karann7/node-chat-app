5307396d00b9904d76d696376bea76f410dc304c
Karan Singh <kar.n@icloud.com>
2017-05-18 22:32:56 -0700

    added the ability to show current users in chat channel

--- app.js
+++ app.js
@@ -3,58 +3,62 @@ const express     = require('express'),
       http        = require('http'),
       socketIO    = require('socket.io'),
       app         = express(),
-      path        = require('path');
+      path        = require('path');  
 // local dependencies
 const port        = process.env.PORT || 3000,
     isRealString  = require('./utils/validation.js'),
       publicPath  = path.join(__dirname, '/public'),
+      {Users}     = require('./utils/users'),
       {generateMessage, generateLocationMessage} = require('./utils/message.js');
 //the express app gets passed in the http method
 //the http method then gets passed into the socket.io
+var users       = new Users();
 const server      = http.createServer(app);
 const io          = socketIO(server);
 //serving public folder
 app.use(express.static(publicPath));
 // All the events must be included in the io.on block
 io.on('connection', (socket)=>{
+  //all the socket code has to be placed inside io.on block.
   console.log('new user connected');
-
   socket.on('join', (params, callback) =>{
     if(!isRealString(params.name) || !isRealString(params.room)) {
-      callback('Name and room name are required.');
+      return callback('Name and room name are required.');
     }
     // join a specific room
     socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
+
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
     //socket.leave to leave the room
-
     //sends welcome message upon page entry
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-
-//sends  message to everyone but the current socket.
-  socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', params.name + ' has joined'));
-
-
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+  //sends  message to everyone but the current socket.
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', params.name + ' has joined'));
+      callback();
+    });
+  //takes a incoming message, and sends it to other connected sockets
+    socket.on('createMessage', (message, callback)=>{
+    console.log(message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
     callback();
-  });
-
-//takes a incoming message, and sends it to other connected sockets
-  socket.on('createMessage', (message, callback)=>{
-  console.log(message);
-  io.emit('newMessage', generateMessage(message.from, message.text));
-  callback();
-  });
-  
-  socket.on('createLocationMessage', (coords)=>{
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
-  });
-  //sending a message to the server on user disconnect
-  socket.on('disconnect', ()=>{
-      console.log('User was disconnected');
-  }); 
+    });
+    //for sending location I made a separate function rather than injecting inline
+    socket.on('createLocationMessage', (coords)=>{
+      io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    });
+    
+    //sending a message to the server on user disconnect
+    socket.on('disconnect', ()=>{
+        var user = users.removeUser(socket.id);
+        if(user){
+          io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+          io.to(user.room).emit('newMessage', generateMessage(user.name, user.name + ' has left.'));
+        }
+    }); 
   //io.on close brace vvvv
 });
-
-
 //server listen
 server.listen(port, ()=>{
       console.log(`SERVER IS RUNNING ON PORT: ${port}`);
